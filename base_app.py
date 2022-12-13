0658f76ffd2e5a6e4995eb1eb1bfee0f0e696718
Marna2014 <111790823+Marna2014@users.noreply.github.com>
2022-12-13 12:15:12 +0200

    Modified streamlit with added pages and pickle file names

--- base_app.py
+++ base_app.py
@@ -29,7 +29,7 @@ import joblib,os
 import pandas as pd
 
 # Vectorizer
-news_vectorizer = open("resources/tfidfvect.pkl","rb")
+news_vectorizer = open("vectoriser.pkl","rb")
 tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl file
 
 # Load your raw data
@@ -41,13 +41,34 @@ def main():
 
 	# Creates a main title and subheader on your page -
 	# these are static across all pages
-	st.title("Tweet Classifer")
+	st.title("Amped Solutions Tweet Classifer")
 	st.subheader("Climate change tweet classification")
+	st.image("Amped_Solutions.png")
 
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
-	options = ["Prediction", "Information"]
+	options = ["Prediction", "Information", "About Us", "Contact Us", "Customer Appreciation"]
 	selection = st.sidebar.selectbox("Choose Option", options)
+
+	# Building out the "Information" page
+	if selection == "Customer Appreciation":
+		st.info("Customer Appreciation about Amped Solutions services")
+		# You can read a markdown file from supporting resources folder
+		st.write("I have been working with Amped Solutions for ten years and I continue to recommend their easy-to-use platform that allows you to discover what people are saying about your brand on twitter all around the word.")
+
+
+	# Building out the "Information" page
+	if selection == "Contact Us":
+		st.info("Contact Information for Amped Solutions")
+		# You can read a markdown file from supporting resources folder
+		st.write("hello@ampedsolutions.com, (011)  666 2345, www.ampetsolutions.co.za  ")
+
+
+	# Building out the "About Us" page
+	if selection == "About Us":
+		st.info("General Information about Amped Solutions")
+		# You can read a markdown file from supporting resources folder
+		st.write("Amped Solutions have created a new platform centred around sentiment analysis. Together we make up a team of experts in data science and machine learning, passionate about making high quality twitter (social media) sentiment analysis so that companies can deliver more effective marketing and achieve favourable results. We have architected a solution with high accuracy and precision. We’re so excited to assist companies in expanding their potential, one tweet at a time…")
 
 	# Building out the "Information" page
 	if selection == "Information":
@@ -70,13 +91,15 @@ def main():
 			vect_text = tweet_cv.transform([tweet_text]).toarray()
 			# Load your .pkl file with the model of your choice + make predictions
 			# Try loading in multiple models to give the user a choice
-			predictor = joblib.load(open(os.path.join("resources/Logistic_regression.pkl"),"rb"))
+			predictor = joblib.load(open(os.path.join("resources/SVC_model.pkl"),"rb"))
 			prediction = predictor.predict(vect_text)
 
 			# When model has successfully run, will print prediction
 			# You can use a dictionary or similar structure to make this output
 			# more human interpretable.
 			st.success("Text Categorized as: {}".format(prediction))
+			st.image("Amped_Solutions.png")
+
 
 # Required to let Streamlit instantiate our web app.  
 if __name__ == '__main__':
